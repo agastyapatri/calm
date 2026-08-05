8cea02842b32cf063f9d4aefcae4ba9ed91e3463
agastyapatri <patriagastya5@gmail.com>
2026-08-05 07:45:48 +0530

    first request sentgitpatgitpat

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector> 
 #include <calm.hpp> 
 #include <string> 
-
+#include <cpr/cpr.h>
 
 int main(){
 	bool status = calm::load_dotenv(".env");
@@ -12,13 +12,37 @@ int main(){
 		std::cerr << "Environment variables not loaded, CALM_STATUS: " << status << "\n";
 	}
 	std::string API_KEY    = std::getenv("OPENROUTER_API_KEY");
-	std::string MODEL_NAME = std::getenv("GEMMA_4_31B");
+	std::string MODEL_NAME = std::getenv("NEMOTRON_3_NANO");
 	std::string BASE_URL   = std::getenv("BASE_URL");
 
-	calm::ChatModel model(API_KEY, MODEL_NAME, BASE_URL);
-	std::cout << model.base_url() << std::endl;
-	std::cout << model.model_name() << std::endl;
-	
+	std::string chat_completion_endpoint = BASE_URL + "chat/completions";
+	json payload = {
+		{"max_tokens", 150}, 
+		{"messages", {
+				{{"content", "You are a helpful assistant."}, {"role", "system"}},
+				{{"content", "What is the capital of france"}, {"role", "user"}},
+
+		}},
+		{"model", MODEL_NAME}, 
+		{"temperature", 0.5}
+	};
+	std::string json_payload = payload.dump(4);
+	cpr::Response r = cpr::Post(
+		cpr::Url(chat_completion_endpoint), 
+		cpr::Header{
+			{"Content-Type", "application/json"},
+			{"Authorization", "Bearer " + API_KEY},
+		},
+		cpr::Body(json_payload)
+	);
+	std::cout << r.text << std::endl;
+
+
+
+
+
+
+
 
 
 
